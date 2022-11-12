ffc64757d60fb296657f1fc297e92b5d45607347
Karol <93782725+petitess@users.noreply.github.com>
2022-11-12 17:21:44 +0100

    Update main.tf

--- xxx01/main.tf
+++ xxx01/main.tf
@@ -1,16 +1,20 @@
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.30.0"
+      version = ">= 3.30.0"
+    #version = "~= 3.30"  for production
     }
   }
 }
 
 provider "azurerm" {
   # Configuration options
+  features {}
 }
 
-resource "" "name" {
-  
+resource "azurerm_resource_group" "main" {
+  name = "rg-terraform-test01"
+  location = "swedencentral"
 }
