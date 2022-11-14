1efdec448dc59616f0c1dfe0cb69b3fdc3fe1ba2
Karol <93782725+petitess@users.noreply.github.com>
2022-11-14 18:23:42 +0100

    Virtual Network with NSG

---
+++ vnet01/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      #version = ">= 3.30.0"
+    #version = "~= 3.30"  for production
+    version = ">= 3.30.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "random" {
+  length = 10
+  upper = false 
+  special = false 
+}
+
