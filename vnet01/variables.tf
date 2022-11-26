ea8c605df0ba46ea301c05694e1999dc601ac373
Karol <93782725+petitess@users.noreply.github.com>
2022-11-26 16:47:22 +0100

    Virtual Network with NSG 10.0.0.0/16

---
+++ vnet01/variables.tf
@@ -0,0 +1,32 @@
+variable "prefix" {
+  type = string
+  default = "infra-test"
+}
+
+variable "env" {
+    description = "Environment"
+    type = string
+    default = "test"
+}
+
+variable "tags" {
+    description = "Tags"
+    type = map(string)
+    default = {
+        Environment = "Test"
+        Application = "Infra"
+    }
+}
+
+variable "app" {
+    description = "Application"
+    type = string
+    default = "Infra"
+}
+
+variable "location" {
+    description = "location"
+    type = string
+    default = "swedencentral"
+}
+
