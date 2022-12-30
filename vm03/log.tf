8fa16c8e3ef542d3d2f4b1cb601f3c2280d522e5
Karol <93782725+petitess@users.noreply.github.com>
2022-12-30 18:06:27 +0100

    Delete vm03 directory

--- vm03/log.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_log_analytics_workspace" "log" {
-    name = "log-${var.prefix}01"
-    location = var.location
-    tags = var.tags
-    resource_group_name = azurerm_resource_group.rginfra.name
-    sku = "PerGB2018"
-    retention_in_days = 90
-}
