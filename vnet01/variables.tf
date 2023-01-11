2fc5ba5d253f90e08aebb5bf022d515bcd19a44e
Karol <93782725+petitess@users.noreply.github.com>
2023-01-11 17:28:04 +0100

    Virtual Network with NSG 10.0.0.0/16

---
+++ vnet01/variables.tf
@@ -0,0 +1,20 @@
+variable "prefix" {}
+variable "env" {}
+variable "location" {}
+variable "tags" {}
+
+variable "vnet" {}
+variable "subnets" {}
+variable "natgatewaysubnets" {}
+
+# variable "nsg-AzureBastion" {}
+# variable "nsg-pe-01" {}
+# variable "nsg-core-01" {}
+# variable "nsg-mgmt-01" {}
+# variable "nsg-sql-01" {}
+# variable "nsg-app-01" {}
+# variable "nsg-web-01" {}
+
+variable "nsg" {}
+
+variable "vms" {}
