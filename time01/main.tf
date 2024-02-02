b4400bf30a2431cbe7608e92c50915033dcdc07a
Karol <93782725+petitess@users.noreply.github.com>
2024-02-02 11:25:17 +0100

    Runbooks from files + link schedules

--- time01/main.tf
+++ time01/main.tf
@@ -13,18 +13,18 @@ locals {
 output "locals" {
   value = {
     "time" = local.time
-    "01"   = "${var.schedules_daily[1].hour}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}"
-    "02"   = "${var.schedules_daily[2].hour}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}"
-    "03"   = "${var.schedules_daily[4].hour}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}"
-    "04"   = local.time >= "${var.schedules_daily[1].hour}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}"
-    "05"   = local.time >= "${var.schedules_daily[2].hour}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}"
-    "06"   = local.time >= "${var.schedules_daily[4].hour}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}"
-    "07" = local.time >= "${var.schedules_daily[1].hour}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[1].hour - 1)}:${var.schedules_daily[1].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[1].hour - 1)}:${var.schedules_daily[1].minute}:00Z"
-    "08" = local.time >= "${var.schedules_daily[2].hour}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[2].hour - 1)}:${var.schedules_daily[2].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[2].hour - 1)}:${var.schedules_daily[2].minute}:00Z"
-    "09" = local.time >= "${var.schedules_daily[4].hour}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[4].hour - 1)}:${var.schedules_daily[4].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[4].hour - 1)}:${var.schedules_daily[4].minute}:00Z"
-    "10" = "${format("hour:%02s", var.schedules_daily[1].hour)}"
-    "11" = "${format("hour:%02s", var.schedules_daily[2].hour)}"
-    "12" = "${format("hour:%02s", var.schedules_daily[4].hour)}"
+    "01"   = "${var.schedules_daily[1].hour -1}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}"
+    "02"   = "${var.schedules_daily[2].hour -1}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}"
+    "03"   = "${var.schedules_daily[4].hour -1}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}"
+    "04"   = local.time >= "${var.schedules_daily[1].hour -1}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}"
+    "05"   = local.time >= "${var.schedules_daily[2].hour -1}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}"
+    "06"   = local.time >= "${var.schedules_daily[4].hour -1}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}"
+    "07" = local.time >= "${var.schedules_daily[1].hour -1}${(var.schedules_daily[1].minute == "00" ? "55" : tonumber(var.schedules_daily[1].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[1].hour - 1)}:${var.schedules_daily[1].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[1].hour - 1)}:${var.schedules_daily[1].minute}:00Z"
+    "08" = local.time >= "${var.schedules_daily[2].hour -1}${(var.schedules_daily[2].minute == "00" ? "55" : tonumber(var.schedules_daily[2].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[2].hour - 1)}:${var.schedules_daily[2].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[2].hour - 1)}:${var.schedules_daily[2].minute}:00Z"
+    "09" = local.time >= "${var.schedules_daily[4].hour -1}${(var.schedules_daily[4].minute == "00" ? "55" : tonumber(var.schedules_daily[4].minute) - 5)}" ? "${local.tomorrowdate}T${format("%02s", var.schedules_daily[4].hour - 1)}:${var.schedules_daily[4].minute}:00Z" : "${local.todaydate}T${format("%02s", var.schedules_daily[4].hour - 1)}:${var.schedules_daily[4].minute}:00Z"
+    "10" = "${format("hour:%02s", var.schedules_daily[1].hour -1)}"
+    "11" = "${format("hour:%02s", var.schedules_daily[2].hour -1)}"
+    "12" = "${format("hour:%02s", var.schedules_daily[4].hour -1)}"
   }
 }
 
