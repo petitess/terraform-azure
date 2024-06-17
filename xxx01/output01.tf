c47187bc780f7d74572ab1c9acc60eaa75b492f5
Karol <93782725+petitess@users.noreply.github.com>
2024-06-17 15:39:31 +0200

    Create output01.tf

---
+++ xxx01/output01.tf
@@ -0,0 +1,31 @@
+locals {
+  env = "dev"
+  sp = {
+    dev = {
+      a1 = "gdg34r53"
+      a2 = "wsfwef"
+      a3 = "yth564t54rtf"
+      a4 = ""
+    }
+  }
+  object = {
+    dev = {
+      a1 = { grp : "gdg34r53", wse : "sdv" }
+      a2 = { grp : "gdg76553", wse : "s34dv" }
+      a3 = { grp : "gd2354r53", wse : "sd53v" }
+      a4 = { grp : "gddr53", wse : "" }
+    }
+  }
+}
+
+output "abc" {
+  value = {
+    for x in local.sp[local.env] : x => x if x != ""
+  }
+}
+
+output "def" {
+  value = {
+    for x in local.object[local.env] : x.grp => x.wse if x.wse != ""
+  }
+}
