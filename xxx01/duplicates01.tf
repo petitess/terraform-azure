5de25462670081d220f076655c66088be6604610
Karol <93782725+petitess@users.noreply.github.com>
2025-02-13 13:54:59 +0100

    distinct, flatten, remove duplicates

---
+++ xxx01/duplicates01.tf
@@ -0,0 +1,29 @@
+locals {
+  my_list = [
+    {
+      city : "London"
+      weather : "sunny"
+    },
+    {
+      city : "Berlin"
+      weather : "rainy"
+    },
+    {
+      city : "Malmo"
+      weather : "rainy"
+    }
+  ]
+}
+
+output "weather" {
+  value = {
+    for s in distinct(flatten(local.my_list.*.weather)) : s => s
+    if s != null
+  }
+}
+
+# OUTPUT:
+# weather = {
+#        rainy = "rainy"
+#        sunny = "sunny"
+#     }
