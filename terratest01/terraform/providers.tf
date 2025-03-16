e69810ba8d4baa2cddfa880b2f689a32eb0d2226
Karol <93782725+petitess@users.noreply.github.com>
2025-03-16 17:32:22 +0100

    test key vault outputs

---
+++ terratest01/terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  backend "azurerm" {
+    container_name   = "tfstate-databricks"
+    key              = "infra-system.terraform.tfstate"
+    use_azuread_auth = true
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.23.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = local.sub_id
+  storage_use_azuread = true
+
+  features {}
+}
