04375a203bf2389d16fa0ce3c18d4b062a61e375
Karol <93782725+petitess@users.noreply.github.com>
2025-09-30 14:00:05 +0200

    cost management export to storage azapi

---
+++ costmanagement02/main.tf
@@ -0,0 +1,196 @@
+terraform {
+  required_version = ">= 1.13.3"
+
+  # backend "azurerm" {
+  #   storage_account_name = "stterraform000001"
+  #   container_name       = "opsgenie-terraform-1-azure"
+  #   key                  = "terraform.tfstate"
+  #   use_azuread_auth     = true
+  # }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      #version = "~= 3.80"  for production
+      version = "~> 4.46.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.7.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id     = local.sub_id
+  storage_use_azuread = true
+  features {}
+}
+
+provider "azapi" {
+  subscription_id = local.sub_id
+}
+
+locals {
+  location     = "swedencentral"
+  sub_id       = "123-b6d8-431f-b2a6-123"
+  now          = timestamp()
+  time         = formatdate("hhmm", local.now)
+  todaydate    = formatdate("YYYY-MM-DD", local.now)
+  end          = timeadd(local.now, "172800h")
+  enddate      = formatdate("YYYY-MM-DD", local.end)
+  tomorrow     = timeadd(local.now, "24h")
+  tomorrowdate = formatdate("YYYY-MM-DD", local.tomorrow)
+}
+
+resource "azurerm_resource_group" "st" {
+  name     = "rg-stcostmanagment01"
+  location = local.location
+}
+
+resource "azurerm_storage_account" "cost" {
+  name                            = "stcostmanagment01"
+  resource_group_name             = azurerm_resource_group.st.name
+  location                        = local.location
+  account_replication_type        = "LRS"
+  account_tier                    = "Standard"
+  account_kind                    = "StorageV2"
+  access_tier                     = "Hot"
+  allow_nested_items_to_be_public = true
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = true
+  shared_access_key_enabled       = false
+  default_to_oauth_authentication = true
+}
+
+resource "azurerm_storage_container" "cost" {
+  name               = "cost"
+  storage_account_id = azurerm_storage_account.cost.id
+}
+
+resource "azapi_resource" "cost_actual" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "cost-export-actual"
+  parent_id = "/subscriptions/${local.sub_id}"
+  identity {
+    type = "SystemAssigned"
+  }
+  location = local.location
+  body = {
+    properties = {
+      definition = {
+        type      = "ActualCost"
+        timeframe = "MonthToDate"
+        # dataset = {
+        #   granularity = "Daily"
+        #   aggregation = {
+        #     totalCost = {
+        #       name     = "PreTaxCost"
+        #       function = "Sum"
+        #     }
+        #   }
+        #   grouping = [
+        #     {
+        #       type = "Dimension"
+        #       name = "ResourceId"
+        #     }
+        #   ]
+        # }
+      }
+      deliveryInfo = {
+        destination = {
+          resourceId     = azurerm_storage_account.cost.id
+          container      = azurerm_storage_container.cost.name
+          rootFolderPath = "exports"
+          type : "AzureBlob"
+        }
+      }
+      compressionMode : "none"
+      format = "Csv"
+      dataOverwriteBehavior : "OverwritePreviousReport"
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "${local.todaydate}T00:00:00Z"
+          to   = local.enddate
+        }
+      }
+    }
+  }
+}
+
+resource "azapi_resource" "cost_usage" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "cost-export-usage"
+  parent_id = "/subscriptions/${local.sub_id}"
+  identity {
+    type = "SystemAssigned"
+  }
+  location = local.location
+  body = {
+    properties = {
+      definition = {
+        type      = "Usage"
+        timeframe = "MonthToDate"
+      }
+      deliveryInfo = {
+        destination = {
+          resourceId     = azurerm_storage_account.cost.id
+          container      = azurerm_storage_container.cost.name
+          rootFolderPath = "exports"
+          type : "AzureBlob"
+        }
+      }
+      compressionMode : "none"
+      format = "Csv"
+      dataOverwriteBehavior : "OverwritePreviousReport"
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "${local.todaydate}T00:00:00Z"
+          to   = local.enddate
+        }
+      }
+    }
+  }
+}
+
+
+resource "azapi_resource" "cost_amortized" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "cost-export-amortized"
+  parent_id = "/subscriptions/${local.sub_id}"
+  identity {
+    type = "SystemAssigned"
+  }
+  location = local.location
+  body = {
+    properties = {
+      definition = {
+        type      = "AmortizedCost"
+        timeframe = "MonthToDate"
+      }
+      deliveryInfo = {
+        destination = {
+          resourceId     = azurerm_storage_account.cost.id
+          container      = azurerm_storage_container.cost.name
+          rootFolderPath = "exports"
+          type : "AzureBlob"
+        }
+      }
+      compressionMode : "none"
+      format = "Csv"
+      dataOverwriteBehavior : "OverwritePreviousReport"
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "${local.todaydate}T00:00:00Z"
+          to   = local.enddate
+        }
+      }
+    }
+  }
+}
