1b2d7959d3230c3787e2219765bc107705a6492f
Karol <93782725+petitess@users.noreply.github.com>
2026-03-07 18:35:19 +0100

    elastic premium, pep, auth

---
+++ functionapp01/func.tf
@@ -0,0 +1,158 @@
+data "azurerm_resource_group" "existing" {
+  for_each = {
+    for key, f in var.function_apps : key => f
+    if try(f.rg_name, null) != null
+  }
+  name = each.value.rg_name
+}
+
+resource "azurerm_resource_group" "func" {
+  for_each = {
+    for key, f in var.function_apps : key => f
+    if try(f.rg_name, null) == null
+  }
+  name     = "rg-${each.key}"
+  location = local.location
+}
+
+resource "azurerm_linux_function_app" "func" {
+  for_each            = var.function_apps
+  name                = each.key
+  resource_group_name = try(azurerm_resource_group.func[each.key].name, data.azurerm_resource_group.existing[each.key].name)
+  location            = local.location
+  identity {
+    type = "SystemAssigned"
+  }
+
+  virtual_network_subnet_id     = azurerm_subnet.subnets["snet-app"].id
+  storage_account_name          = azurerm_storage_account.st_map[each.value.st_name].name
+  storage_uses_managed_identity = true
+  service_plan_id               = azurerm_service_plan.func.id
+  https_only                    = true
+  site_config {
+    application_stack {
+      python_version              = try(each.value.python_version, null)
+      dotnet_version              = try(each.value.dotnet_version, null)
+      java_version                = try(each.value.java_version, null)
+      node_version                = try(each.value.node_version, null)
+      powershell_core_version     = try(each.value.powershell_core_version, null)
+      use_custom_runtime          = try(each.value.use_custom_runtime, null)
+      use_dotnet_isolated_runtime = try(each.value.use_dotnet_isolated_runtime, null)
+    }
+    cors {
+      support_credentials = false
+      allowed_origins     = ["https://portal.azure.com"]
+    }
+    http2_enabled                          = false
+    remote_debugging_enabled               = false
+    websockets_enabled                     = false
+    ftps_state                             = "Disabled"
+    minimum_tls_version                    = "1.2"
+    vnet_route_all_enabled                 = true
+    always_on                              = true
+    use_32_bit_worker                      = false
+    pre_warmed_instance_count              = 1
+    application_insights_connection_string = try(sensitive(azurerm_application_insights.appi.connection_string), null)
+    ip_restriction_default_action          = "Allow"
+    scm_ip_restriction_default_action      = "Allow"
+  }
+
+  app_settings = merge({
+    AzureFunctionsWebHost__hostid = replace(uuidv5("oid", "${each.key}.azurewebsites.net"), "-", "")
+  }, try(each.value.app_settings, null))
+
+  dynamic "auth_settings_v2" {
+    for_each = try(each.value.auth_settings_v2, null) != null ? [each.value.auth_settings_v2] : []
+    content {
+      auth_enabled                            = try(auth_settings_v2.value.auth_enabled, null)
+      unauthenticated_action                  = try(auth_settings_v2.value.unauthenticated_action, null)
+      forward_proxy_convention                = try(auth_settings_v2.value.forward_proxy_convention, null)
+      http_route_api_prefix                   = try(auth_settings_v2.value.http_route_api_prefix, null)
+      require_authentication                  = try(auth_settings_v2.value.require_authentication, null)
+      require_https                           = try(auth_settings_v2.value.require_https, null)
+      runtime_version                         = try(auth_settings_v2.value.runtime_version, null)
+      excluded_paths                          = try(auth_settings_v2.value.excluded_paths, null)
+      default_provider                        = try(auth_settings_v2.value.default_provider, null)
+      forward_proxy_custom_host_header_name   = try(auth_settings_v2.value.forward_proxy_custom_host_header_name, null)
+      forward_proxy_custom_scheme_header_name = try(auth_settings_v2.value.forward_proxy_custom_scheme_header_name, null)
+      active_directory_v2 {
+        client_secret_setting_name      = try(auth_settings_v2.value.client_secret_setting_name, null)
+        allowed_applications            = try(auth_settings_v2.value.allowed_applications, null)
+        allowed_groups                  = try(auth_settings_v2.value.allowed_groups, null)
+        allowed_identities              = try(auth_settings_v2.value.allowed_identities, null)
+        client_id                       = try(auth_settings_v2.value.client_id, null)
+        jwt_allowed_client_applications = try(auth_settings_v2.value.jwt_allowed_client_applications, null)
+        jwt_allowed_groups              = try(auth_settings_v2.value.jwt_allowed_groups, null)
+        login_parameters                = try(auth_settings_v2.value.login_parameters, null)
+        tenant_auth_endpoint            = try(auth_settings_v2.value.tenant_auth_endpoint, null)
+        www_authentication_disabled     = try(auth_settings_v2.value.www_authentication_disabled, null)
+        allowed_audiences               = try(auth_settings_v2.value.allowed_audiences, null)
+      }
+      login {
+        token_refresh_extension_time      = try(auth_settings_v2.value.token_refresh_extension_time, null)
+        token_store_enabled               = try(auth_settings_v2.value.token_store_enabled, null)
+        token_store_path                  = try(auth_settings_v2.value.token_store_path, null)
+        allowed_external_redirect_urls    = try(auth_settings_v2.value.allowed_external_redirect_urls, null)
+        cookie_expiration_convention      = try(auth_settings_v2.value.cookie_expiration_convention, null)
+        cookie_expiration_time            = try(auth_settings_v2.value.cookie_expiration_time, null)
+        logout_endpoint                   = try(auth_settings_v2.value.logout_endpoint, null)
+        nonce_expiration_time             = try(auth_settings_v2.value.nonce_expiration_time, null)
+        preserve_url_fragments_for_logins = try(auth_settings_v2.value.preserve_url_fragments_for_logins, null)
+        token_store_sas_setting_name      = try(auth_settings_v2.value.token_store_sas_setting_name, null)
+        validate_nonce                    = try(auth_settings_v2.value.validate_nonce, null)
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags["hidden-link: /app-insights-resource-id"]]
+  }
+}
+
+resource "azurerm_private_endpoint" "func-linux" {
+  for_each                      = var.function_apps
+  name                          = "pep-${azurerm_linux_function_app.func[each.key].name}"
+  location                      = local.location
+  resource_group_name           = azurerm_linux_function_app.func[each.key].resource_group_name
+  subnet_id                     = azurerm_subnet.subnets["snet-pep"].id
+  custom_network_interface_name = "nic-${azurerm_linux_function_app.func[each.key].name}"
+  private_service_connection {
+    name                           = "con-${azurerm_linux_function_app.func[each.key].name}"
+    private_connection_resource_id = azurerm_linux_function_app.func[each.key].id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+  ip_configuration {
+    name               = "ipconfig"
+    member_name        = "sites"
+    private_ip_address = each.value.pep_ip_address
+    subresource_name   = "sites"
+  }
+
+  private_dns_zone_group {
+    name                 = "sites"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns[index(local.pdnsz, "privatelink.azurewebsites.net")].id]
+  }
+}
+
+resource "azurerm_role_assignment" "func" {
+  for_each             = var.function_apps
+  scope                = try(azurerm_resource_group.func[each.key].id, data.azurerm_resource_group.existing[each.key].id)
+  principal_id         = azurerm_linux_function_app.func[each.key].identity[0].principal_id
+  role_definition_name = "Storage Blob Data Contributor"
+}
+
+resource "azurerm_role_assignment" "func_mon" {
+  for_each             = var.function_apps
+  scope                = try(azurerm_resource_group.func_commmon.id, data.azurerm_resource_group.existing[each.key].id)
+  principal_id         = azurerm_linux_function_app.func[each.key].identity[0].principal_id
+  role_definition_name = "Monitoring Metrics Publisher"
+}
+
+resource "azurerm_role_assignment" "func_kv" {
+  for_each             = var.function_apps
+  scope                = try(azurerm_resource_group.func[each.key].id, data.azurerm_resource_group.existing[each.key].id)
+  principal_id         = azurerm_linux_function_app.func[each.key].identity[0].principal_id
+  role_definition_name = "Key Vault Administrator"
+}
---
+++ functionapp01/variables.tf
@@ -0,0 +1,154 @@
+variable "env" {
+  type    = string
+  default = "dev"
+}
+variable "subid" {
+  type    = string
+  default = "abc"
+}
+
+variable "st_map" {
+  type = map(object({
+    name                          = string
+    rg_name                       = optional(string)
+    account_kind                  = string
+    account_replication_type      = string
+    access_tier                   = string
+    public_network_access_enabled = bool
+    allowed_ips                   = list(string)
+    containers                    = optional(list(string))
+    shares                        = optional(list(string))
+    queues                        = optional(list(string))
+    tables                        = optional(list(string))
+    private_endpoints = optional(object({
+      blob  = optional(string)
+      file  = optional(string)
+      table = optional(string)
+      queue = optional(string)
+    }))
+  }))
+
+  default = {
+    "stfuncrepublik01" = {
+      name                          = "stfuncrepublik01"
+      rg_name                       = "ex=rg-func-republik01"
+      account_kind                  = "StorageV2"
+      account_replication_type      = "LRS"
+      access_tier                   = "Hot"
+      public_network_access_enabled = true
+      allowed_ips                   = ["1.150.118.1"]
+      containers                    = ["con12", "con34"]
+      shares                        = ["func-code"]
+      tables                        = ["t12", "t34"]
+      private_endpoints = {
+        blob = "10.1.1.132"
+        file = "10.1.1.133"
+      }
+    }
+  }
+}
+
+variable "function_apps" {
+  type = map(object({
+    rg_name                     = optional(string)
+    st_name                     = string
+    python_version              = optional(string)
+    dotnet_version              = optional(string)
+    java_version                = optional(string)
+    node_version                = optional(string)
+    powershell_core_version     = optional(string)
+    use_custom_runtime          = optional(string)
+    use_dotnet_isolated_runtime = optional(string)
+    pep_ip_address              = string
+    app_settings                = optional(map(string))
+    auth_settings_v2 = optional(object({
+      auth_enabled                            = optional(bool)
+      unauthenticated_action                  = optional(string)
+      forward_proxy_convention                = optional(string)
+      http_route_api_prefix                   = optional(string)
+      require_authentication                  = optional(bool)
+      require_https                           = optional(bool)
+      runtime_version                         = optional(string)
+      excluded_paths                          = optional(list(string))
+      default_provider                        = optional(string)
+      forward_proxy_custom_host_header_name   = optional(string)
+      forward_proxy_custom_scheme_header_name = optional(string)
+      client_secret_setting_name              = optional(string)
+      allowed_applications                    = optional(list(string))
+      allowed_groups                          = optional(list(string))
+      allowed_identities                      = optional(list(string))
+      client_id                               = string
+      jwt_allowed_client_applications         = optional(list(string))
+      jwt_allowed_groups                      = optional(list(string))
+      login_parameters                        = optional(map(string))
+      tenant_auth_endpoint                    = string
+      www_authentication_disabled             = optional(bool)
+      allowed_audiences                       = optional(list(string))
+      token_refresh_extension_time            = optional(number)
+      token_store_enabled                     = optional(bool)
+      token_store_path                        = optional(string)
+      allowed_external_redirect_urls          = optional(list(string))
+      cookie_expiration_convention            = optional(string)
+      cookie_expiration_time                  = optional(string)
+      logout_endpoint                         = optional(string)
+      nonce_expiration_time                   = optional(string)
+      preserve_url_fragments_for_logins       = optional(bool)
+      token_store_sas_setting_name            = optional(string)
+      validate_nonce                          = optional(bool)
+    }))
+  }))
+  default = {
+    "func-republik01" = {
+      st_name        = "stfuncrepublik01"
+      python_version = "3.11"
+      pep_ip_address = "10.1.1.134"
+      app_settings = {
+        "APPLICATIONINSIGHTS_AUTHENTICATION_STRING" = "Authorization=AAD"
+        "BUILD_FLAGS"                               = "UseExpressBuild"
+        "ENABLE_ORYX_BUILD"                         = "true"
+        "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"  = "@Microsoft.KeyVault(VaultName=kv-infra-abc-01;SecretName=MySecret)"
+        "SCM_DO_BUILD_DURING_DEPLOYMENT"            = "1"
+        "WEBSITE_AUTH_AAD_ALLOWED_TENANTS"          = "xyz"
+        "XDG_CACHE_HOME"                            = "/tmp/.cache"
+        #IF WEBSITE CONTENT SHOULD BE ON A SHARE USE SETTINGS BELOW:
+        #vnetContentShareEnabled IS NOT AVAILABLE IN THIS PROVIDER
+        # "WEBSITE_CONTENTOVERVNET" = 1
+        # "WEBSITE_CONTENTSHARE"    = "func-code"
+        # WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = "xyz"
+      }
+      auth_settings_v2 = {
+        auth_enabled                          = false
+        unauthenticated_action                = "Return403"
+        forward_proxy_convention              = "NoProxy"
+        http_route_api_prefix                 = "/.auth"
+        require_authentication                = true
+        require_https                         = true
+        runtime_version                       = "~1"
+        excluded_paths                        = ["/api/health"]
+        forward_proxy_custom_host_header_name = "X-Original-Host"
+        client_secret_setting_name            = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
+        allowed_applications                  = ["123456-abcdef-xyz"]
+        client_id                             = "123456-abcdef-xyz"
+        tenant_auth_endpoint                  = "https://sts.windows.net/{tenant_id}/v2.0"
+        www_authentication_disabled           = false
+        allowed_audiences                     = ["api://123456-abcdef-xyz"]
+        token_store_enabled                   = true
+      }
+    }
+    "func-republik002" = {
+      rg_name        = "rg-func-republik01"
+      st_name        = "stfuncrepublik01"
+      python_version = "3.11"
+      pep_ip_address = "10.1.1.135"
+      app_settings = {
+        "APPLICATIONINSIGHTS_AUTHENTICATION_STRING" = "Authorization=AAD"
+        "BUILD_FLAGS"                               = "UseExpressBuild"
+        "ENABLE_ORYX_BUILD"                         = "true"
+        "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"  = "@Microsoft.KeyVault(VaultName=kv-infra-abc-01;SecretName=MySecret)"
+        "SCM_DO_BUILD_DURING_DEPLOYMENT"            = "1"
+        "WEBSITE_AUTH_AAD_ALLOWED_TENANTS"          = "xyz"
+        "XDG_CACHE_HOME"                            = "/tmp/.cache"
+      }
+    }
+  }
+}
